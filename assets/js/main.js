25a5d27632d5960e1a205cda7d11130c78829d49
Berk Altıok <berkpw@gmail.com>
2019-11-02 11:22:13 +0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,80 @@
 const router = new VueRouter({
   mode: 'history',
-  // base: '/Portfolio/',
+  base: '/InstaBulk/',
   routes: []
 });
 const app = new Vue({
   router,
+  components: {
+    'vue-recaptcha': VueRecaptcha
+  },
   data() {
     return {
-      loader: true
+      loader: true,
+      wait: true,
+      ad: false,
+      bulks: "",
+      bulksList: [],
+      button: {
+        text: 'Şimdi Sonuçlar Listele',
+        icon: 'search',
+        color: 'purple',
+        click: false
+      }
+    }
+  },
+  methods: {
+    checkBulk: function() {
+      const that = this;
+
+      // Check Area
+      let list = this.bulks
+        .replace(" ","")
+        .split("\n")
+        .filter(e => e !== "")
+        .filter((v, i, a) => a.indexOf(v) === i);
+      that.bulksList = [];
+      list.forEach(function (e) {
+        that.bulksList.push({
+          nick: e,
+          type: "wait",
+          usable: false,
+          possible: true
+        });
+      });
+
+      // Actions
+      if (this.bulksList[0]) {
+        $('html, body').animate({
+          scrollTop: $(".content").offset().top
+        }, 1000, "swing", function () {
+          that.wait = false;
+        });
+        $.each(that.bulksList, function (i,e) {
+          axios({
+            url: 'http://localhost/ideasoft/instagram.php',
+            responseType: 'json',
+            params: {
+              nick: e.nick
+            },
+          }).then(function (response) {
+            that.bulksList[i].usable = response.data.usable;
+            that.bulksList[i].possible = response.data.possible;
+            that.bulksList[i].type = response.data.possible ? response.data.usable ? "check" : "times" : "exclamation";
+          });
+        });
+      } else {
+        this.changeButton('Boş Kontrol Yapılamaz', 'times', 'danger');
+      }
+    },
+    changeButton: function (text, icon, color = 'purple', time = 1500) {
+      const that = this, defaultButton = this.button;
+      if (!this.button.click) {
+        this.button = {text: text, icon: icon, color: color, click: true};
+        setTimeout(function () {
+          that.button = defaultButton;
+        }, time);
+      }
     }
   },
   mounted() {
